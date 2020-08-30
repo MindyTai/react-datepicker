7dee1d6c1b760ab0a61e338446afd0788cab7870
Mindy <mindytai175@gmail.com>
2020-08-30 11:31:40 +0800

    feat: add basic setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from '../public/logo.png';
+
+const App = () => (
+  <section>
+    <h2>Welcome to the app!</h2>
+    <img src={logo} alt="logo" />
+  </section>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: ['./src/index.jsx'],
+  output: {
+    filename: '[name].[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env',
+              '@babel/preset-react',
+            ],
+          },
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][hash].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      favicon: './public/favicon.png',
+    }),
+    new WorkboxPlugin.GenerateSW({
+      clientsClaim: true,
+      skipWaiting: true,
+      exclude: [/\.(?:png|jpg|jpeg|svg)$/],
+      runtimeCaching: [{
+        urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+        handler: 'CacheFirst',
+        options: {
+          cacheName: 'images',
+          expiration: {
+            maxEntries: 10,
+          },
+        },
+      }],
+    }),
+  ],
+  resolve: {
+    extensions: ['.jsx', '.js'],
+  },
+  devServer: {
+    port: 8000,
+    writeToDisk: true,
+    hot: true,
+    contentBase: './dist',
+  },
+};
