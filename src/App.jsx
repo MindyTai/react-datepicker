db8918c9b19eb02e644105765c33313f1ebcb95d
mindy <mindytai175@gmail.com>
2021-05-27 19:48:02 +0800

    feat: remove redundant sample files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
 
-import logo from '../public/logo.png';
-import './style/main.scss';
-
 const App = () => (
     <section>
-        <h2 className="text-primary">Welcome to the app!</h2>
-        <img src={logo} alt="logo" />
+        <h2 className="text-primary">Welcome to the calender app!</h2>
     </section>
 );
 
