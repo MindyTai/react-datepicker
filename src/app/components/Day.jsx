2c0eb50c43510c90f7fa0bb6f02f5e571cb775cb
mindy <mindytai175@gmail.com>
2021-05-29 13:21:13 +0800

    fix: add selected current day

--- src/app/components/Day.jsx
+++ src/app/components/Day.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 import Proptypes from 'prop-types';
 import classNames from 'classnames';
 
-export const Day = ({ day, isSelected, handleClick }) => (
-    <button className={ classNames('w-13 h-13 focus:outline-none rounded-full', { 'bg-red-400 text-white': isSelected }) } onClick={ handleClick }>
+export const Day = ({
+  day, isSelected, handleClick, isCurrentMonth,
+}) => (
+    <button
+      className={ classNames('w-13 h-13 focus:outline-none rounded-full',
+        { 'bg-red-400 text-white': isSelected && isCurrentMonth }, { 'text-gray-400': !isCurrentMonth }) }
+      onClick={ handleClick }
+      disabled={ !isCurrentMonth }>
       { day }
     </button>
 );
@@ -12,4 +18,5 @@ Day.propTypes = {
   day: Proptypes.number,
   isSelected: Proptypes.bool,
   handleClick: Proptypes.func,
+  isCurrentMonth: Proptypes.bool,
 };
--- src/app/components/Days.jsx
+++ src/app/components/Days.jsx
@@ -1,26 +1,33 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+import _ from 'lodash';
 
 import { Day } from './Day';
 import { CalenderContext } from '../../App';
 
 export const Days = ({ days }) => {
   const {
-    month, startDay, day: currentDay, dispatch,
+    month, startDay, day: currentDay, date, dispatch,
   } = useContext(CalenderContext);
-  const daysOfTheMonth = Array(days[month] + startDay - 1).fill(null);
+  const lastMonthDays = _.rangeRight(days[month - 1], days[month - 1] - startDay);
+  const currentMonthDays = _.range(1, days[month] + 1, 1);
+  const nextMonthDaysCount = 7 * 6 - lastMonthDays.length - currentMonthDays.length;
+  const nextMonthDays = _.range(1, nextMonthDaysCount + 1, 1);
+  const currentMonthIndexs = _.range(startDay, days[month] + startDay, 1);
 
+  const daysOfTheMonth = [...lastMonthDays, ...currentMonthDays, ...nextMonthDays];
+  console.log(date.getMonth(), month);
   return (
     <div className="border flex flex-wrap text-center">
-      { daysOfTheMonth.map((_, idx) => {
-        const day = idx - startDay + 1;
-        return <Day key={ idx }
-                    day={ day > 0 ? day : '' }
-                    isSelected={ day === currentDay }
-                    handleClick={ () => {
-                      dispatch({ type: 'SET_SELECTED_DATE', payload: day });
-                    } }/>;
-      }) }
+      { daysOfTheMonth.map((day, idx) => (
+         <Day key={ idx }
+              day={ day }
+              isSelected={ day === currentDay }
+              isCurrentMonth={ !!currentMonthIndexs.includes(idx) }
+              handleClick={ () => {
+                dispatch({ type: 'SET_SELECTED_DATE', payload: day });
+              } }
+          />)) }
     </div>
   );
 };
